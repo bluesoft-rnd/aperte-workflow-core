561b919821951b5ea53b40d73cc2e29e895e17f7
Tomek Lipski <tomek.lipski@gmail.com>
2012-01-24 16:00:19 +0100

    added todo annotation

--- core/base-gui/src/main/java/org/aperteworkflow/portlets/ProcessConsolePortlet.java
+++ core/base-gui/src/main/java/org/aperteworkflow/portlets/ProcessConsolePortlet.java
@@ -4,6 +4,7 @@ import org.aperteworkflow.ui.processmanager.ProcessDefinitionManagerPane;
 import pl.net.bluesoft.rnd.util.vaadin.GenericVaadinPortlet2BpmApplication;
 
 /**
+ * TODO
  * @author tlipski@bluesoft.net.pl
  */
 public class ProcessConsolePortlet extends GenericVaadinPortlet2BpmApplication {
@@ -13,6 +14,6 @@ public class ProcessConsolePortlet extends GenericVaadinPortlet2BpmApplication {
 
         @Override
         protected void renderPortlet() {
-            getMainWindow().setContent(new ProcessDefinitionManagerPane(this));
+//            getMainWindow().setContent(new ProcessDefinitionManagerPane(this));
         }
 }

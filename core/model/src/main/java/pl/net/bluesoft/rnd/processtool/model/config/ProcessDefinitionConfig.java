3d0a5b3753000220df18388ffce7e4c77bae503c
Piotrek <polszewski@bluesoft.net.pl>
2013-08-21 14:32:21 +0200

    fix

--- core/model/src/main/java/pl/net/bluesoft/rnd/processtool/model/config/ProcessDefinitionConfig.java
+++ core/model/src/main/java/pl/net/bluesoft/rnd/processtool/model/config/ProcessDefinitionConfig.java
@@ -45,7 +45,7 @@ public class ProcessDefinitionConfig extends AbstractPersistentEntity {
 			parameters = {
 					@org.hibernate.annotations.Parameter(name = "initial_value", value = "" + 1),
 					@org.hibernate.annotations.Parameter(name = "value_column", value = "_DB_ID"),
-					@org.hibernate.annotations.Parameter(name = "sequence_name", value = "DB_SEQ_ID_PROC_QUEUE_CONF")
+					@org.hibernate.annotations.Parameter(name = "sequence_name", value = "DB_SEQ_ID_PROC_DEF_CONF")
 			}
 	)
 	@Column(name = "id")
@@ -82,10 +82,12 @@ public class ProcessDefinitionConfig extends AbstractPersistentEntity {
 	 */
 	private boolean latest;
 
+	@Override
 	public Long getId() {
 		return id;
 	}
 
+	@Override
 	public void setId(Long id) {
 		this.id = id;
 	}

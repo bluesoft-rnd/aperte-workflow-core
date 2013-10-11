59d128493268348e03ee3e03cab81adbb694215c
Filip Karpinski <fkarpinski@bluesoft.net.pl>
2013-10-11 10:10:44 +0200

    ProcessDiagramWidget

---
+++ plugins/base-widgets/src/main/java/pl/net/bluesoft/rnd/processtool/ui/basewidgets/ProcessDiagramWidget.java
@@ -0,0 +1,21 @@
+package pl.net.bluesoft.rnd.processtool.ui.basewidgets;
+
+import pl.net.bluesoft.rnd.processtool.plugins.IBundleResourceProvider;
+import pl.net.bluesoft.rnd.processtool.ui.widgets.ProcessHtmlWidget;
+import pl.net.bluesoft.rnd.processtool.ui.widgets.annotations.AliasName;
+import pl.net.bluesoft.rnd.processtool.ui.widgets.annotations.WidgetGroup;
+import pl.net.bluesoft.rnd.processtool.web.widgets.impl.FileWidgetContentProvider;
+
+
+@AliasName(name = "ProcessDiagramWidget")
+@WidgetGroup("html-process-widget")
+public class ProcessDiagramWidget extends ProcessHtmlWidget {
+	
+	public ProcessDiagramWidget(IBundleResourceProvider bundleResourceProvider) {
+		setWidgetName("ProcessDiagramWidget");
+        setContentProvider(new FileWidgetContentProvider("process-diagram.html", bundleResourceProvider));
+		
+    }
+}
+
+

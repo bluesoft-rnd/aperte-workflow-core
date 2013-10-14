cf506a28a43fe0d8861850dc59d20bb7ae26810e
Piotrek <polszewski@bluesoft.net.pl>
2013-10-14 15:30:58 +0200

    post merge fixes

--- plugins/base-widgets/src/main/java/pl/net/bluesoft/rnd/processtool/ui/basewidgets/ProcessDataBlockHtmlWidget.java
+++ plugins/base-widgets/src/main/java/pl/net/bluesoft/rnd/processtool/ui/basewidgets/ProcessDataBlockHtmlWidget.java
@@ -1,30 +1,16 @@
 package pl.net.bluesoft.rnd.processtool.ui.basewidgets;
 
 import com.vaadin.ui.TextArea;
-
 import pl.net.bluesoft.rnd.processtool.plugins.IBundleResourceProvider;
-import pl.net.bluesoft.rnd.processtool.ui.basewidgets.editor.ScriptCodeEditor;
 import pl.net.bluesoft.rnd.processtool.ui.widgets.ProcessHtmlWidget;
-import pl.net.bluesoft.rnd.processtool.ui.widgets.annotations.AliasName;
-import pl.net.bluesoft.rnd.processtool.ui.widgets.annotations.AperteDoc;
-import pl.net.bluesoft.rnd.processtool.ui.widgets.annotations.AutoWiredProperty;
-import pl.net.bluesoft.rnd.processtool.ui.widgets.annotations.AutoWiredPropertyConfigurator;
-import pl.net.bluesoft.rnd.processtool.ui.widgets.annotations.ChildrenAllowed;
-import pl.net.bluesoft.rnd.processtool.ui.widgets.annotations.WidgetGroup;
+import pl.net.bluesoft.rnd.processtool.ui.widgets.annotations.*;
 import pl.net.bluesoft.rnd.processtool.web.widgets.impl.FileWidgetContentProvider;
 
 @AperteDoc(humanNameKey = "widget.process_data_block_html.name", descriptionKey = "widget.process_data_block.description")
 @ChildrenAllowed(false)
 @WidgetGroup("base-widgets")
 @AliasName(name = "ProcessDataHtml")
-public class ProcessDataBlockHtmlWidget extends ProcessHtmlWidget /*BaseProcessToolVaadinWidget implements ProcessToolDataWidget, ProcessToolVaadinRenderable*/ {
-
-	public ProcessDataBlockHtmlWidget(IBundleResourceProvider bundleResourceProvider)
-    {
-        setWidgetName("ProcessDataHtml");
-        setContentProvider(new FileWidgetContentProvider("html-process.html", bundleResourceProvider));
-    }
-	
+public class ProcessDataBlockHtmlWidget extends ProcessHtmlWidget {
     @AutoWiredProperty(required = true)
     @AutoWiredPropertyConfigurator(fieldClass = TextArea.class)
     @AperteDoc(
@@ -32,5 +18,9 @@ public class ProcessDataBlockHtmlWidget extends ProcessHtmlWidget /*BaseProcessT
             descriptionKey = "widget.process_data_block.property.widgetsDefinition.description"
     )
     private String widgetsDefinition;
-    
+
+	public ProcessDataBlockHtmlWidget(IBundleResourceProvider bundleResourceProvider)
+	{
+		setContentProvider(new FileWidgetContentProvider("html-process.html", bundleResourceProvider));
+	}
 }

068feee4601f0d0b152ee6773edcdcd6331608ad
Maciej Pawlak <inz.pawlak@gmail.com>
2014-05-10 10:33:04 +0200

    style and set variable step fix

--- plugins/base-widgets/src/main/java/pl/net/bluesoft/rnd/processtool/ui/basewidgets/steps/SetVariablesStep.java
+++ plugins/base-widgets/src/main/java/pl/net/bluesoft/rnd/processtool/ui/basewidgets/steps/SetVariablesStep.java
@@ -1,11 +1,13 @@
 package pl.net.bluesoft.rnd.processtool.ui.basewidgets.steps;
 
 
+import pl.net.bluesoft.rnd.processtool.ProcessToolContext;
 import pl.net.bluesoft.rnd.processtool.model.BpmStep;
 import pl.net.bluesoft.rnd.processtool.model.ProcessInstance;
 import pl.net.bluesoft.rnd.processtool.steps.ProcessToolProcessStep;
 import pl.net.bluesoft.rnd.processtool.ui.widgets.annotations.AliasName;
 import pl.net.bluesoft.rnd.processtool.ui.widgets.annotations.AutoWiredProperty;
+import pl.net.bluesoft.rnd.util.StepUtil;
 
 import java.util.Map;
 import java.util.logging.Logger;
@@ -26,6 +28,7 @@ public class SetVariablesStep implements ProcessToolProcessStep {
     {
 
     	ProcessInstance pi = step.getProcessInstance();
+        ProcessToolContext ctx = ProcessToolContext.Util.getThreadProcessToolContext();
 
     	if("true".equals(applyToRoot))
     		pi = pi.getRootProcessInstance();
@@ -43,7 +46,7 @@ public class SetVariablesStep implements ProcessToolProcessStep {
     			assignment[1] = assignment[1].substring(1, assignment[1].length() - 1);
 
             String key = assignment[0];
-            String value = assignment[1];
+            String value = StepUtil.extractVariable(assignment[1], ctx, pi);
             pi.setSimpleAttribute(key, value);
 
     	}

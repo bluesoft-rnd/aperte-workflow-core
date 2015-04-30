4cbc73e30e085be1dcdcdfa3ae331b1bba09bd93
kmruk <kmruk@bluesoft.net.pl>
2015-04-30 12:53:09 +0200

    merge zapytania BpmTaskQuery

--- core/jbpm-context/src/main/java/pl/net/bluesoft/rnd/pt/ext/jbpm/service/query/BpmTaskQuery.java
+++ core/jbpm-context/src/main/java/pl/net/bluesoft/rnd/pt/ext/jbpm/service/query/BpmTaskQuery.java
@@ -75,6 +75,8 @@ public class BpmTaskQuery {
     private QueueOrderCondition sortField;
     private QueueOrder sortOrder;
 
+
+
     private int offset;
     private int limit = -1;
 
@@ -239,6 +241,149 @@ public class BpmTaskQuery {
         }
         else {
             sb.append("process.*, task_.id as taskId, task_.actualowner_id as assignee, ");
+            sb.append("NULL as groupId, ");
+            sb.append("i18ntext_.shortText as taskName, task_.createdOn as createdOn, task_.completedOn as completedOn, ");
+            sb.append("task_.status as taskStatus, process.definition_id as definitionId, ");
+            sb.append(DEADLINE_SUBQUERY);
+            sb.append("AS taskDeadline, stepInfo_.message AS stepInfo");
+        }
+
+        String castTypeName = hibernateDialect.getCastTypeName(Types.VARCHAR);
+        sb.append(" FROM pt_process_instance process JOIN Task task_ ON CAST(task_.processinstanceid AS "+castTypeName+" ) = process.internalId ");
+        sb.append(" LEFT JOIN PeopleAssignments_PotOwners potowners on potowners.task_id = task_.id AND potowners.entity_id = :user ");
+        sb.append(" LEFT JOIN pt_process_instance_owners powner on powner.process_id = process.id AND powner.owners = :user ");
+
+        queryParameters.add(new QueryParameter("user", user));
+
+        if (taskNames != null || queryType == QueryType.LIST || hasText(searchExpression)) {
+            sb.append(" JOIN I18NText i18ntext_ ON i18ntext_.task_names_id = task_.id");
+        }
+
+        if (excludedDefinitionIds != null && !excludedDefinitionIds.isEmpty()) {
+            sb.append(" JOIN pt_process_definition_config def ON def.id = process.definition_id");
+        }
+
+        if (queryType == QueryType.LIST) {
+            sb.append(" LEFT JOIN pt_step_info stepInfo_ ON stepInfo_.taskId = task_.id");
+        }
+
+        /* Add additional join conditions */
+        //for(SortingOrder sortingOrder: columnSorting)
+        //    sb.append(queryConditions.getSortJoinCondition(sortingOrder.getColumnName()));
+
+        //sb.append(queryConditions.getJoin());
+
+        sb.append(" WHERE 1=1");
+
+        if (queues != null)
+        {
+            sb.append(" AND potowners.entity_id IN (:queues) ");
+            sb.append(" AND task_.actualowner_id IS NULL");
+            sb.append(" AND task_.status NOT IN ('Completed')");
+            queryParameters.add(new QueryParameter("queues", queues));
+        }
+
+        if (owners != null) {
+            sb.append(" AND owners IN (:owners)");
+            queryParameters.add(new QueryParameter("owners", owners));
+        }
+
+        if (virtualQueues != null && user != null) {
+            sb.append(from(virtualQueues).select(GET_VIRTUAL_QUEUES).toString(" OR ", " AND (", ")"));
+        }
+
+        if (taskNames != null) {
+            sb.append(" AND i18ntext_.shortText IN (:taskNames)");
+            queryParameters.add(new QueryParameter("taskNames", taskNames));
+        }
+
+        if (createdBefore != null) {
+            sb.append(" AND task_.createdOn <= :createdBefore");
+            queryParameters.add(new QueryParameter("createdBefore", createdBefore));
+        }
+
+        if (createdAfter != null) {
+            sb.append(" AND task_.createdOn >= :createdAfter");
+            queryParameters.add(new QueryParameter("createdAfter", createdAfter));
+        }
+
+        if (hasText(processBpmKey)) {
+            sb.append(" AND process.definitionname = :processBpmKey");
+            queryParameters.add(new QueryParameter("processBpmKey", processBpmKey));
+        }
+
+        //sb.append(queryConditions.getWhereCondition());
+
+        if (hasText(searchExpression)) {
+            sb.append(" AND (");
+            sb.append("task_.actualowner_id LIKE '%' || :expression || '%'");
+//            sb.append(" OR process.creatorLogin LIKE '%' || :expression || '%'");
+//            sb.append(" OR (CASE WHEN process.externalKey IS NOT NULL THEN process.externalKey ELSE process.internalId END) LIKE '%' || :expression || '%'");
+//            sb.append(" OR to_char(task_.createdOn, 'YYYY-MM-DD HH24:MI:SS') LIKE :expression || '%'");
+//            sb.append(" OR EXISTS(SELECT 1 FROM pt_process_instance_s_attr attr");
+//            sb.append("	WHERE attr.process_instance_id = process.id AND attr.value_ LIKE '%' || :expression || '%')");
+//            sb.append(" OR to_char(");
+//            sb.append(DEADLINE_SUBQUERY);
+//            sb.append(", 'YYYY-MM-DD HH24:MI:SS') LIKE :expression || '%'");
+            //sb.append(" OR " + queryConditions.getSearchCondition());
+//
+            queryParameters.add(new QueryParameter("expression", searchExpression.trim()));
+
+            List<Long> definitionDescrKeys = getSearchKeywordMatchingIds(getThreadProcessToolContext()
+                    .getProcessDefinitionDAO().getProcessDefinitionDescriptions());
+
+            if (!definitionDescrKeys.isEmpty()) {
+                sb.append(" OR process.definition_id IN (:searchProcessDefIds)");
+
+                queryParameters.add(new QueryParameter("searchProcessDefIds", definitionDescrKeys));
+            }
+
+            List<Long> stateDescrKeys = getSearchKeywordMatchingIds(getThreadProcessToolContext()
+                    .getProcessDefinitionDAO().getProcessStateDescriptions());
+
+            if (!stateDescrKeys.isEmpty()) {
+                sb.append(" OR EXISTS(SELECT 1 FROM pt_process_state_config psc WHERE psc.id IN (:searchProcessStateIds)");
+                sb.append(" AND psc.definition_id = process.definition_id AND psc.name = i18ntext_.shortText)");
+
+                queryParameters.add(new QueryParameter("searchProcessStateIds", stateDescrKeys));
+            }
+
+            sb.append(')');
+        }
+
+        if (excludedDefinitionIds != null && !excludedDefinitionIds.isEmpty()) {
+            sb.append(" AND (def.bpmDefinitionKey || '_' || def.bpmDefinitionVersion) NOT IN (:excludedDefinitionIds)");
+
+            queryParameters.add(new QueryParameter("excludedDefinitionIds", excludedDefinitionIds));
+        }
+
+        if (queryType == QueryType.LIST && locale != null) {
+            sb.append(" AND (stepInfo_.locale IS NULL OR stepInfo_.locale = :locale)");
+
+            queryParameters.add(new QueryParameter("locale", locale.getLanguage()));
+        }
+
+        if (queryType == QueryType.LIST) {
+            //sb.append(getOrderCondition());
+            sb.append(" ORDER BY ").append(getOrder());
+        }
+        String txt = sb.toString();
+        log.info(txt);
+        return txt;
+    }
+
+
+    /*
+    private String getQueryString(QueryType queryType, List<QueryParameter> queryParameters) {
+        StringBuilder sb = new StringBuilder(queryType == QueryType.COUNT ? 512 : 3 * 512);
+
+        sb.append("SELECT ");
+
+        if (queryType == QueryType.COUNT) {
+            sb.append("COUNT(*)");
+        }
+        else {
+            sb.append("process.*, task_.id as taskId, task_.actualowner_id as assignee, ");
             sb.append("CASE WHEN task_.actualowner_id IS NULL THEN potowners.entity_id END as groupId, ");
             sb.append("i18ntext_.shortText as taskName, task_.createdOn as createdOn, task_.completedOn as completedOn, ");
             sb.append("task_.status as taskStatus, process.definition_id as definitionId, ");
@@ -249,7 +394,7 @@ public class BpmTaskQuery {
         String castTypeName = hibernateDialect.getCastTypeName(Types.VARCHAR);
         sb.append(" FROM pt_process_instance process JOIN Task task_ ON CAST(task_.processinstanceid AS "+castTypeName+" ) = process.internalId");
 
-        /* Queue or all user tasks */
+        // Queue or all user tasks
         if (queues != null || queryType == QueryType.LIST || (virtualQueues != null && virtualQueues.contains(QueueType.ALL_TASKS)))
         {
         	if (hasText(searchExpression)) {
@@ -380,7 +525,7 @@ public class BpmTaskQuery {
         log.info(txt);
         return txt;
     }
-
+*/
     private String getOrder() {
         if (sortField != null) {
             return getOrderField() + ' ' + getOrderDirection();
@@ -429,6 +574,21 @@ public class BpmTaskQuery {
             case ALL_TASKS:
                 return "(((potowners.entity_id = :user AND task_.status NOT IN ('Reserved')) OR task_.actualowner_id = :user) AND task_.status NOT IN ('Completed'))";
             case MY_TASKS:
+                return "(((potowners.entity_id = :user AND task_.status NOT IN ('Reserved')) OR task_.actualowner_id = :user) AND task_.status NOT IN ('Completed'))";
+            case OWN_IN_PROGRESS:
+                return "((process.creatorLogin = :user OR (owners IN (:user))) AND task_.status NOT IN ('Completed') AND (task_.actualowner_id != :user OR task_.actualowner_id is null))";
+            case OWN_FINISHED:
+                return "(process.creatorLogin = :user AND task_.actualowner_id = :user AND task_.status IN ('Completed'))";
+            default:
+                throw new RuntimeException("Unhandled type: " + virtualQueue);
+        }
+    }
+    /*
+    private static String getVirtualQueueCondition(QueueType virtualQueue) {
+        switch (virtualQueue) {
+            case ALL_TASKS:
+                return "(((potowners.entity_id = :user AND task_.status NOT IN ('Reserved')) OR task_.actualowner_id = :user) AND task_.status NOT IN ('Completed'))";
+            case MY_TASKS:
                 return "(task_.actualowner_id = :user AND task_.status NOT IN ('Completed'))";
             case OWN_IN_PROGRESS:
                 return "(process.creatorLogin = :user AND task_.status NOT IN ('Completed'))";
@@ -438,6 +598,7 @@ public class BpmTaskQuery {
                 throw new RuntimeException("Unhandled type: " + virtualQueue);
         }
     }
+    */
 
     private List<Long> getSearchKeywordMatchingIds(Map<Long, String> i18NKeys) {
         if (locale == null) {
